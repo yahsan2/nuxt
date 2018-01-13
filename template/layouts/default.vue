c7e4c7084384394e05d8ba1c47fd496ce7c8ed0c
nekosaur <albert@kaaman.nu>
2018-01-13 15:38:29 +0100

    fix: added .sync modifier to mini-variant
    closes #22

--- template/layouts/default.vue
+++ template/layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      :mini-variant="miniVariant"
+      :mini-variant.sync="miniVariant"
       :clipped="clipped"
       v-model="drawer"
       fixed
       app
     >
       <v-list>
-        <v-list-tile 
+        <v-list-tile
           router
           :to="item.to"
           :key="i"
@@ -26,7 +26,7 @@
     </v-navigation-drawer>
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
+      <v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
